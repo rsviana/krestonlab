9adf0e51ee530f792acad923ed6130a288f087bd
rsviana <rsviana@gmail.com>
2026-06-06 09:42:22 -0300

    Adicionando mais lamboratorio

--- krestonlab/labs.py
+++ krestonlab/labs.py
@@ -3,24 +3,45 @@ LABS = {
         "image": "vulnerables/web-dvwa",
         "internal_port": 80,
         "default_port": 8089,
-        "description": "Damn Vulnerable Web Application"
+        "description": "Damn Vulnerable Web Application",
+        "env": {},
+        "volumes": []
     },
+
     "Mutillidae": {
-        "description": "OWASP Mutillidae II",
         "image": "citizenstig/nowasp",
+        "internal_port": 80,
         "default_port": 8081,
-        "internal_port": 80
+        "description": "OWASP Mutillidae II",
+        "env": {},
+        "volumes": []
     },
+
     "WebGoat": {
         "image": "szsecurity/webgoat",
         "internal_port": 80,
         "default_port": 8082,
-        "description": "OWASP WebGoat"
+        "description": "OWASP WebGoat",
+        "env": {},
+        "volumes": []
     },
+
     "bWAPP": {
-        "image": "citizenstig/nowasp",
+        "image": "raesene/bwapp",
         "internal_port": 80,
         "default_port": 8083,
+        "description": "Buggy Web Application",
+        "env": {},
+        "volumes": []
+    },
+
+    "JuiceShop": {
+        "image": "bkimminich/juice-shop",
+        "internal_port": 3000,
+        "default_port": 8084,
+        "description": "OWASP Juice Shop",
+        "env": {},
+        "volumes": []
     }
 }
 # krestonlab - http://rodrigoviana.dev.br
