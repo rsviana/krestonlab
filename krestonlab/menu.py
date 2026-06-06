f1bd05c5ade76b2e3baa995e4dfe39e975808d30
rsviana <rsviana@gmail.com>
2026-06-06 10:10:07 -0300

    Adicionando mais lamboratorio

--- krestonlab/menu.py
+++ krestonlab/menu.py
@@ -21,8 +21,8 @@ def banner():
     console.print(
         Panel.fit(
             "[bold red]KRESTONLAB - v.2 ☠️ More Machines ☠️[/bold red]\n\n"
-            "[bold]Offensive Security Local Lab Manager[/bold]\n"
-            "KrestonLab é um laboratório para CyberSecurity. Use-o sem moderação.",
+            "[bold]Offensive Security Local Lab Manager [/bold]\n"
+            "KrestonLab é um laboratório para CyberSecurity. Use-o sem moderação. ",
             border_style="red"
         )
     )
